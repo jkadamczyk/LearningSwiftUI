7b53e6bebdc8d188a10585472d3b911f0f547bef
Jakub Adamczyk <adamczyk.jk@gmail.com>
2020-12-17 21:05:22 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jakub Adamczyk on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
