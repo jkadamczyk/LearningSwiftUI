022167caf79039bf380db7595a61da067366ab34
Jakub Adamczyk <adamczyk.jk@gmail.com>
2021-01-01 20:43:32 +0100

    navigation implemented without passing data

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Jakub Adamczyk on 01/01/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarksList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
