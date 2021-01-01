a445ca1dca230c0efe677021de6c9843f7806feb
Jakub Adamczyk <adamczyk.jk@gmail.com>
2021-01-01 20:28:59 +0100

    section 1 and 2 of second lesson

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Jakub Adamczyk on 01/01/2021.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height:50)
+            Text(landmark.name)
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
